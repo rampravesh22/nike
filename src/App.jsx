34ce3d7a0da958e93415fe3b51099ffd1da4a4d5
rpc <rpc7863@gamil.com>
2023-08-27 14:44:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import CustomerReviews from "./sections/CustomerReviews";
+import Footer from "./sections/Footer";
+import Hero from "./sections/Hero";
+import Nav from "./components/Nav";
+import PopularProducts from "./sections/PopularProducts";
+import Services from "./sections/Services";
+import SpecialOffer from "./sections/SpecialOffer";
+import Subscribe from "./sections/Subscribe";
+import SuperQuality from "./sections/SuperQuality";
+
+function App() {
+	return (
+		<main>
+			<Nav />
+			<section className="xl:padding-1 wide:padding-r padding-b">
+				<Hero />
+			</section>
+			<section className="padding">
+				<PopularProducts />
+			</section>
+			<section className="padding">
+				<SuperQuality />
+			</section>
+			<section className="padding-x py-10">
+				<Services />
+			</section>
+			<section className="padding">
+				<SpecialOffer />
+			</section>
+			<section className="padding bg-pale-blue">
+				<CustomerReviews />
+			</section>
+			<section className="padding padding-x sm:py-32 py-16 w-full">
+				<Subscribe />
+			</section>
+			<section className="padding bg-black padding-x padding-t pb-8">
+				<Footer />
+			</section>
+		</main>
+	);
+}
+
+export default App;
